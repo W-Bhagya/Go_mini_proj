4b05d63e9988468c898877258cdd9e95dc8e8fdf
W-Bhagya <91195191+W-Bhagya@users.noreply.github.com>
2022-07-22 22:55:08 +0530

    Add files via upload

---
+++ web_server.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Println(w,"ParseForm() err : %v",err)
+		return
+	}
+
+	fmt.Println(w,"POST request successful")
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	empid := r.FormValue("empid")
+	address := r.FormValue("address")
+
+
+	fmt.Fprintf(w, "Name = %s\n",name)
+	fmt.Fprintf(w, "Email = %s\n",email)
+	fmt.Fprintf(w, "EmpId = %s\n",empid)
+	fmt.Fprintf(w, "Address = %s\n",address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w,"Hello....!")
+}
+
+
+func main() {
+
+	// checks static directory
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+	
+	fmt.Println("Starting server at port 8080")
+
+	if err := http.ListenAndServe(":8080",nil); err != nil {
+		log.Fatal(err)
+	}
+}
